479050670e04d011774a97354fc5ba9b2b81484f
Rasujon3 <rasujon3@gmail.com>
2022-03-02 22:03:56 +0600

    settimeout()

---
+++ timeout.js
@@ -0,0 +1,14 @@
+function doSomething() {
+  console.log("I am coding js");
+}
+console.log("First: ami sobar age");
+console.log("Second: I an the second person");
+// setTimeout(doSomething, 6000);
+setTimeout(function () {
+  console.log("I am using vs code");
+}, 5000);
+setTimeout(() => {
+  console.log("Exploring MDN articles");
+}, 4000);
+console.log("Third: I an the third person");
+console.log("Fourth: I an the fourth person");
