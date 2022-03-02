a1d0bcfd5eade2a46619a293f8b47fa2c9358609
Rasujon3 <rasujon3@gmail.com>
2022-03-03 00:30:01 +0600

    done

---
+++ setinterval.js
@@ -0,0 +1,17 @@
+console.log("first");
+// const timeIntervalId = setInterval(() => {
+//   console.log("tik tik tik");
+// }, 1000);
+
+let seconds = 0;
+
+const timeId = setInterval(() => {
+  //   seconds++;
+  console.log(++seconds);
+  //   console.log(seconds);
+  if (seconds > 15) {
+    clearInterval(timeId);
+  }
+}, 1000);
+
+console.log("second");
