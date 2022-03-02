fa375a77d8535a74bb8ead36b2c89ad94dd93931
Rasujon3 <rasujon3@gmail.com>
2022-03-02 22:20:12 +0600

    38-5 Recognize fetch as an Asynchronous activity

--- timeout.js
+++ timeout.js
@@ -3,12 +3,12 @@ function doSomething() {
 }
 console.log("First: ami sobar age");
 console.log("Second: I an the second person");
-// setTimeout(doSomething, 6000);
-setTimeout(function () {
-  console.log("I am using vs code");
-}, 5000);
-setTimeout(() => {
-  console.log("Exploring MDN articles");
-}, 4000);
+setTimeout(doSomething, 6000);
+// setTimeout(function () {
+//   console.log("I am using vs code");
+// }, 5000);
+// setTimeout(() => {
+//   console.log("Exploring MDN articles");
+// }, 4000);
 console.log("Third: I an the third person");
 console.log("Fourth: I an the fourth person");
